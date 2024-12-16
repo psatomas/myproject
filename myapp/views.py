eb2e1a3b17b9499a7adfae4c20aa8dccdd21ee7d
psatomas <psatomas@gmail.com>
2024-12-16 20:29:44 -0300

    feat: add post page and update counter logic

--- myapp/views.py
+++ myapp/views.py
@@ -55,6 +55,8 @@ def logout(request):
     return redirect('/')
 
 def counter(request):
-    text = request.POST['text']
-    amount_of_words = len(text.split())
-    return render(request, 'counter.html', {'amount': amount_of_words})
+    posts = [1, 2, 3, 4, 5, 'tim', 'tom', 'john']
+    return render(request, 'counter.html', {'posts': posts})
+
+def post(request, pk):
+    return render(request, 'post.html', {'pk': pk})
